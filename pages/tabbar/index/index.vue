3867d93df2babcfb343d3d3d3b772f0d3207d9b6
mazili <white_mzl@163.com>
2020-07-17 17:05:29 +0800

    新建uniapp项目环境

---
+++ pages/tabbar/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="content">
+	<!-- 自定义导航栏 -->
+			<navbar></navbar>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
